95021b3da16bf7f633fa25c6a4e02bee8077aecb
louloudev <louloudevelop@gmail.com>
2019-08-15 20:37:48 +0800

    Problem: title is hidden in mobile view
    Solution: modify the style to make it shown on mobile's header view

--- src/components/ProfileContent/index.js
+++ src/components/ProfileContent/index.js
@@ -16,8 +16,9 @@ const useStyles = makeStyles(theme => ({
   },
   bigAvatar: {
     margin: 10,
-    width: '30%',
-    height: '30%',
+    minWidth: 200,
+    width: '20%',
+    height: '20%',
   },
 }))
 
@@ -27,6 +28,12 @@ export default function SwitchListSecondary() {
     <>
       <List className={classes.root}>
         <Grid container justify="center" alignItems="center">
+          <ListItem>
+            <ListItemText
+              id="switch-list-label-darkMode"
+              primary="User Name Here"
+            />
+          </ListItem>
           <Avatar
             alt="Remy Sharp"
             src="https://pbs.twimg.com/profile_images/712703916358537217/mcOketun_400x400.jpg"
@@ -34,6 +41,8 @@ export default function SwitchListSecondary() {
           />
         </Grid>
       </List>
+
+      <Divider />
       <List
         subheader={<ListSubheader>Bio</ListSubheader>}
         className={classes.root}
