24c59ae6894e6aebbc2b57956ade40607acdeb89
louloudev <louloudevelop@gmail.com>
2019-08-21 17:56:52 +0800

    Problem: some unused files remaining
    Solution: remove the unused files

--- src/components/GeneralLayout/index.js
+++ src/components/GeneralLayout/index.js
@@ -1,19 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ThemeProvider, theme } from 'util/style'
-import GlobalStyles from 'util/style/GlobalStyles'
-
 import GeneralHeader from 'components/GeneralHeader'
 import config from '../../../config/meta'
 
 const Layout = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <>
-      <GlobalStyles />
-      <GeneralHeader siteTitle={config.siteTitle || config.siteTitleAlt} />
-      {children}
-    </>
-  </ThemeProvider>
+  <>
+    <GeneralHeader siteTitle={config.siteTitle || config.siteTitleAlt} />
+    {children}
+  </>
 )
 
 Layout.propTypes = {
--- src/components/Layout/index.jsx
+++ src/components/Layout/index.jsx
@@ -1,21 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { ThemeProvider, theme } from 'util/style'
-import GlobalStyles from 'util/style/GlobalStyles'
 
 import Header from 'components/Header'
 import Footer from 'components/Footer'
+import Toolbar from '@material-ui/core/Toolbar'
 import config from '../../../config/meta'
 
 const Layout = ({ children }) => (
-  <ThemeProvider theme={theme}>
-    <>
-      <GlobalStyles />
-      <Header siteTitle={config.siteTitle || config.siteTitleAlt} />
-      <div style={{ minHeight: '100vh', paddingTop: 56 }}>{children}</div>
-      <Footer />
-    </>
-  </ThemeProvider>
+  <>
+    <Header siteTitle={config.siteTitle || config.siteTitleAlt} />
+    <Toolbar />
+    <div style={{ minHeight: '100vh' }}>{children}</div>
+    <Footer />
+  </>
 )
 
 Layout.propTypes = {
