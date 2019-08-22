4c0dd34da45330c3b3cbbe03bfc1881ccf786ff8
louloudev <louloudevelop@gmail.com>
2019-08-22 13:38:21 +0800

    Problem: Type script not available
    Solution: add type-script plugin

--- src/pages/chat.jsx
+++ src/pages/chat.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ChatLayout from 'components/ChatLayout'
 import SEO from 'components/SEO'
-import Chat from 'components/Chat'
+import Chat from 'components/Chat/index'
 
 const ChatPage = () => (
   <ChatLayout>
