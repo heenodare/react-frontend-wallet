6aadaa24f0e4737fe956327858398d64499edd88
louloudev <louloudevelop@gmail.com>
2019-08-27 17:23:24 +0800

    Problem: chat Input doesn't have a way to input image/video/audio
    Solution: add a menu to let user choose the message type

--- src/components/ChatInput/index.jsx
+++ src/components/ChatInput/index.jsx
@@ -6,6 +6,8 @@ import InputBase from '@material-ui/core/InputBase'
 import IconButton from '@material-ui/core/IconButton'
 import MenuIcon from '@material-ui/icons/Menu'
 import SendIcon from '@material-ui/icons/Send'
+import MenuItem from '@material-ui/core/MenuItem'
+import Menu from '@material-ui/core/Menu'
 
 const useStyles = makeStyles({
   appBar: {
@@ -35,26 +37,62 @@ const useStyles = makeStyles({
 function ChatInput() {
   const classes = useStyles()
 
+  const [anchorEl, setAnchorEl] = React.useState(null)
+
+  const isMenuOpen = Boolean(anchorEl)
+
+  function handleInputMenuOpen(event) {
+    setAnchorEl(event.currentTarget)
+  }
+
+  function handleMenuClose() {
+    setAnchorEl(null)
+  }
+
+  const menuId = 'primary-search-account-menu'
+  const renderMenu = (
+    <Menu
+      anchorEl={anchorEl}
+      anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
+      id={menuId}
+      keepMounted
+      transformOrigin={{ vertical: 'top', horizontal: 'right' }}
+      open={isMenuOpen}
+      onClose={handleMenuClose}
+    >
+      <MenuItem onClick={handleMenuClose}>Image</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Video</MenuItem>
+      <MenuItem onClick={handleMenuClose}>Audio</MenuItem>
+    </Menu>
+  )
+
   return (
-    <AppBar position="sticky" color="primary" className={classes.appBar}>
-      <Paper square className={classes.root}>
-        <IconButton className={classes.iconButton} aria-label="menu">
-          <MenuIcon />
-        </IconButton>
-        <InputBase
-          className={classes.input}
-          placeholder="Your Message Here..."
-          inputProps={{ 'aria-label': 'Message Input' }}
-        />
-        <IconButton
-          className={classes.iconButton}
-          aria-label="send"
-          style={{ marginLeft: 'auto' }}
-        >
-          <SendIcon />
-        </IconButton>
-      </Paper>
-    </AppBar>
+    <>
+      <AppBar position="sticky" color="primary" className={classes.appBar}>
+        <Paper square className={classes.root}>
+          <IconButton
+            className={classes.iconButton}
+            aria-label="menu"
+            onClick={handleInputMenuOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+          <InputBase
+            className={classes.input}
+            placeholder="Your Message Here..."
+            inputProps={{ 'aria-label': 'Message Input' }}
+          />
+          <IconButton
+            className={classes.iconButton}
+            aria-label="send"
+            style={{ marginLeft: 'auto' }}
+          >
+            <SendIcon />
+          </IconButton>
+        </Paper>
+      </AppBar>
+      {renderMenu}
+    </>
   )
 }
 
